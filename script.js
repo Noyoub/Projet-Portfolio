6f7837330e018a57ac9db1f6416e3c0e400ea84a
Noyoub <lucas.bouillon@laposte.net>
2021-02-11 21:26:29 +0100

    modification du script

--- script.js
+++ script.js
@@ -9,35 +9,27 @@ function myFunction() {
 }
 
 //Gestion affichage des descriptions des projets
-var img1 = document.getElementById('img1');
-var content1 = document.getElementById('content1');
+var containers = document.getElementsByClassName('container2');
+var contents = document.getElementsByClassName('content2');
 
-var img2 = document.getElementById('img2');
-var content2 = document.getElementById('content2');
 
-var img3 = document.getElementById('img3');
-var content3 = document.getElementById('content3');
+containers[0].addEventListener("mouseenter", function(){ 
+  contents[0].style.visibility="visible"; });
+containers[0].addEventListener("mouseleave", function(){ 
+  contents[0].style.visibility="hidden"; });
 
-var img4 = document.getElementById('img4');
-var content4 = document.getElementById('content4');
+containers[1].addEventListener("mouseenter", function(){ 
+  contents[1].style.visibility="visible"; });
+containers[1].addEventListener("mouseleave", function(){ 
+  contents[1].style.visibility="hidden"; });
 
-img1.addEventListener("mouseenter", function(){ 
-  content1.style.visibility="visible"; });
-img1.addEventListener("mouseleave", function(){ 
-  content1.style.visibility="hidden"; });
+containers[2].addEventListener("mouseenter", function(){ 
+  contents[2].style.visibility="visible"; });
+containers[2].addEventListener("mouseleave", function(){ 
+  contents[2].style.visibility="hidden"; });
 
-img2.addEventListener("mouseenter", function(){ 
-  content2.style.visibility="visible"; });
-img2.addEventListener("mouseleave", function(){ 
-  content2.style.visibility="hidden"; });
-
-img3.addEventListener("mouseenter", function(){ 
-  content3.style.visibility="visible"; });
-img3.addEventListener("mouseleave", function(){ 
-  content3.style.visibility="hidden"; });
-
-img4.addEventListener("mouseenter", function(){ 
-  content4.style.visibility="visible"; });
-img4.addEventListener("mouseleave", function(){ 
-  content4.style.visibility="hidden"; });
+containers[3].addEventListener("mouseenter", function(){ 
+  contents[3].style.visibility="visible"; });
+containers[3].addEventListener("mouseleave", function(){ 
+  contents[3].style.visibility="hidden"; });
 
