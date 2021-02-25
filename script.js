23dfc75c6b460af417d84ed3d70d9cd591a7b4be
Noyoub <lucas.bouillon@laposte.net>
2021-02-25 13:49:32 +0100

    VF

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Gestion du menu responsive
 
-var x = document.querySelector("header ul");
+var x = document.querySelector("nav ul");
 
 function myFunction() {
   if (x.style.display === "flex") {
@@ -15,34 +15,48 @@ function myFunction2() {
     x.style.display = "none";
 }
 
-//Animation titre
+//Animations de la section "Header"
 
 const txtAnim = document.querySelector('.title h2');
 
 new Typewriter(txtAnim, {
-  deleteSpeed: 30
+  deleteSpeed: 10
 })
 .changeDelay(30)
 .typeString('Bienvenue sur mon portfolio !')
-.pauseFor(300)
+.pauseFor(1000)
 .deleteChars(29)
 .typeString('Je suis étudiant en informatique, passionné du développement web.')
 .start()
 
 gsap.registerPlugin(ScrollTrigger);
 
-//Animations de la section "about"
-
 window.addEventListener("load", () => {
   const TL = gsap.timeline();
 
   TL
-  .staggerTo(".title h1", 1, { top: 0, opacity: 1, ease: "back.out(8.7)" }, 0.2)
-  .staggerFrom(".about_img img", 1, { right: -500, opacity: 0, ease: "power2.out" }, 0.2, "-=1")
-  .staggerTo(".about_img img", 1, { opacity: 1}, 0.2, "-=1")
+  .staggerTo(".title h1", 1, { top: 0, opacity: 1, ease: "back.out(3.7)" }, 0.2)
   .staggerTo(".title h2", 1, { right: 0, opacity: 1, ease: "back.out(1.7)" }, 0.2)
   .staggerTo(".dl_cv", 1, { opacity: 1, ease: "power2.out" }, 0.2, "-=1");
 });
+
+
+
+//Animations de la section "skills"
+
+var descrips_skills = document.querySelectorAll(".responsive_box p");
+var skills_boxs = document.querySelectorAll('.responsive_box');
+
+for (let index = 0; index < descrips_skills.length; index++) {
+  skills_boxs[index].addEventListener('mouseenter', event => {
+    if(window.innerWidth >= 1080)
+      descrips_skills[index].style.filter = "invert(100%)";
+  })
+  skills_boxs[index].addEventListener('mouseleave', event => {
+    if(window.innerWidth >= 1080)
+      descrips_skills[index].style.filter = "invert(0)";
+  })
+}
 
 //Animations de la section "portfolio"
 
@@ -58,4 +72,16 @@ tl_portfolio
 .to(".c3", { y: 0, opacity: 1, duration: 0.5, ease: "back.out(1.7)"})
 .to(".c4", { y: 0, opacity: 1, duration: 0.5, ease: "back.out(1.7)"}, "-=0.5")
 
+//Animations de la section "footer
+
+var tl_footer = gsap.timeline({
+  scrollTrigger: {
+    trigger: "footer a"
+  }
+});
+
+tl_footer
+.to("#gt_logo", { y: 0, opacity: 1, duration: 0.3, ease: "back.out(0.7)"})
+.to("#ig_logo", { y: 0, opacity: 1, duration: 0.3, ease: "back.out(0.7)"}, "-=0.2")
+.to("#ld_logo", { y: 0, opacity: 1, duration: 0.3, ease: "back.out(0.7)"}, "-=0.2");
 
